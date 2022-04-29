7e62c0373cf19ecf12e5f1646148fe9d8451b752
MrLinoyk <freejase@bk.ru>
2022-04-29 13:59:10 +0300

    Answer

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -2,19 +2,20 @@ package koschei;
 
 import koschei.models.Ocean1;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KoscheiTheDeathless {
-
-    private Ocean1 ocean;
+    @Autowired
+    private Ocean1 ocean1;
 
     public String getRulesByDeth() {
-        return "На свете есть океан , " + ocean.toString();
+        return "На свете есть океан , " + ocean1.toString();
     }
 
     @Autowired
     public void setOcean(Ocean1 ocean) {
-        this.ocean = ocean;
+        this.ocean1 = ocean;
     }
 }
